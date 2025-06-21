2a113f9cb2593e1e4ed124fb8c0ce3a88c94bfdf
Alex Temnok <temnok@gmail.com>
2025-06-21 16:09:44 -0700

    Refactor

--- exec_compute_m.go
+++ exec_compute_m.go
@@ -5,40 +5,40 @@ func (cpu *CPU) execInstrComputeMul(rs2, rs1, f3, rd int32) {
 	var c int32
 
 	switch f3 {
-	case 0: // mul
+	case 0b_000: // mul
 		c = a * b
 
-	case 1: // mulh
+	case 0b_001: // mulh
 		c = int32(int64(a) * int64(b) >> 32)
 
-	case 2: // mulhsu
+	case 0b_010: // mulhsu
 		c = int32(int64(a) * int64(uint32(b)) >> 32)
 
-	case 3: // mulhu
+	case 0b_011: // mulhu
 		c = int32(int64(uint32(a)) * int64(uint32(b)) >> 32)
 
-	case 4: // div
+	case 0b_100: // div
 		if b != 0 {
 			c = a / b
 		} else {
 			c = -1
 		}
 
-	case 5: // divu
+	case 0b_101: // divu
 		if b != 0 {
 			c = int32(uint32(a) / uint32(b))
 		} else {
 			c = -1
 		}
 
-	case 6: // rem
+	case 0b_110: // rem
 		if b != 0 {
 			c = a % b
 		} else {
 			c = a
 		}
 
-	case 7: // remu
+	case 0b_111: // remu
 		if b != 0 {
 			c = int32(uint32(a) % uint32(b))
 		} else {
