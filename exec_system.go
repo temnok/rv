1639c71dc8face59b3ad93e082264d3673c5682c
Alex Temnok <temnok@gmail.com>
2025-06-22 05:13:17 -0700

    Implement breakpoint

--- exec_system.go
+++ exec_system.go
@@ -1,8 +1,6 @@
 package rv
 
 func (cpu *CPU) execSystem(imm, rs1, f3, rd int32) {
-	imm = bits(imm, 0, 12)
-
 	if f3 == 0 {
 		cpu.execSystemSpecial(imm, rd)
 	} else {
@@ -20,6 +18,9 @@ func (cpu *CPU) execSystemSpecial(imm, rd int32) {
 	case 0b_0000_000_00000: // ecall
 		cpu.trap(ExceptionEnvironmentCallFromMMode)
 
+	case 0b_0000_000_00001: // ebreak
+		cpu.trap(ExceptionBreakpoint)
+
 	case 0b_0001_000_00010: // sret
 		cpu.ret(PrivS)
 
@@ -32,6 +33,8 @@ func (cpu *CPU) execSystemSpecial(imm, rd int32) {
 }
 
 func (cpu *CPU) execSystemCSR(imm, rs1, f3, rd int32) {
+	csr := bits(imm, 0, 12)
+
 	s := rs1
 	if (f3 & 0b_100) == 0 {
 		s = cpu.x[s]
@@ -41,17 +44,17 @@ func (cpu *CPU) execSystemCSR(imm, rs1, f3, rd int32) {
 
 	switch f3 & 0b_11 {
 	case 0b_01: // csrrw
-		if (rd == 0 || cpu.csrRead(imm, &val)) && cpu.csrWrite(imm, s) {
+		if (rd == 0 || cpu.csrRead(csr, &val)) && cpu.csrWrite(csr, s) {
 			cpu.x[rd] = val
 		}
 
 	case 0b_10: // csrrs
-		if cpu.csrRead(imm, &val) && (s == 0 || cpu.csrWrite(imm, val|s)) {
+		if cpu.csrRead(csr, &val) && (s == 0 || cpu.csrWrite(csr, val|s)) {
 			cpu.x[rd] = val
 		}
 
 	case 0b_11: // csrrc
-		if cpu.csrRead(imm, &val) && (s == 0 || cpu.csrWrite(imm, val&^s)) {
+		if cpu.csrRead(csr, &val) && (s == 0 || cpu.csrWrite(csr, val&^s)) {
 			cpu.x[rd] = val
 		}
 
