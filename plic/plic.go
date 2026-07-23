e5cad910e0068240e3e0789a3a16543f2b85966f
Alex Temnok <temnok@gmail.com>
2026-07-23 02:24:35 -0700

    Refactor PLIC

--- plic/plic.go
+++ plic/plic.go
@@ -11,7 +11,6 @@ type PLIC struct {
 
 	pending int
 	enable  int
-	claim   int
 }
 
 func New(cpu *state.CPU) *PLIC {
@@ -27,23 +26,22 @@ func (plic *PLIC) Access(addr int, width int, write bool, writeData int) int {
 
 	switch addr {
 	case 0x1000: // pending
-		val = plic.pending
+		if !write {
+			val = plic.pending
+		}
 
 	case 0x2000: // enable
-		val = plic.enable
-
 		if write {
 			plic.enable = writeData
 			plic.sync()
+		} else {
+			val = plic.enable
 		}
 
-	case 0x200004: // claim
-		val = plic.claim
-
-		if write {
-			plic.claim = writeData
-		} else {
-			plic.pending &^= 1 << plic.claim
+	case 0x20_0000, 0x20_0004: // claim
+		if !write {
+			val = bits.TrailingZeros(uint(plic.pending&plic.enable)) & 63
+			plic.pending &^= 1 << val
 		}
 
 		plic.sync()
@@ -52,11 +50,11 @@ func (plic *PLIC) Access(addr int, width int, write bool, writeData int) int {
 	return val
 }
 
-func (plic *PLIC) PendInterrupt(source int, enable bool) {
-	if mask := 1 << source; enable {
-		plic.pending |= mask
+func (plic *PLIC) PendInterrupt(interruptID int, enable bool) {
+	if enable {
+		plic.pending |= 1 << interruptID
 	} else {
-		plic.pending &^= mask
+		plic.pending &^= 1 << interruptID
 	}
 
 	plic.sync()
@@ -64,10 +62,8 @@ func (plic *PLIC) PendInterrupt(source int, enable bool) {
 
 func (plic *PLIC) sync() {
 	if active := plic.pending & plic.enable; active != 0 {
-		plic.claim = bits.TrailingZeros(uint(active))
 		plic.cpu.CSR.Mip |= 1 << csr.MipSEIP
 	} else {
-		plic.claim = 0
 		plic.cpu.CSR.Mip &^= 1 << csr.MipSEIP
 	}
 }
