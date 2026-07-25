bd3c97ea366e3024d6de0c5fbaed112317b45a3f
Alex Temnok <temnok@gmail.com>
2026-07-24 21:20:11 -0700

    Refactor UART

--- uart/uart.go
+++ uart/uart.go
@@ -7,8 +7,6 @@ import (
 
 type UART struct {
 	cpu *state.CPU
-
-	reg int
 }
 
 func New(cpu *state.CPU) *UART {
@@ -18,14 +16,16 @@ func New(cpu *state.CPU) *UART {
 }
 
 func (uart *UART) Access(addr int, width int, write bool, writeVal int) int {
+	cpu := uart.cpu
+
 	var val int
 
 	switch addr {
 	case 0x0200_0000: // txdata
-		val = (^uart.reg >> csr.UartIP) & 1 << 31
+		val = (^cpu.CSR.Uart >> csr.UartIP) & 1 << 31
 
 		if write {
-			uart.reg = uart.reg&^(1<<csr.UartIP|0xFF<<csr.UartTX) | writeVal&0xFF<<csr.UartTX
+			cpu.CSR.Uart = cpu.CSR.Uart&^(1<<csr.UartIP|0xFF<<csr.UartTX) | writeVal&0xFF<<csr.UartTX
 
 			uart.sync()
 		}
@@ -33,27 +33,27 @@ func (uart *UART) Access(addr int, width int, write bool, writeVal int) int {
 	case 0x0200_0004: // rxdata
 		val = 1 << 31
 
-		if !write && (uart.reg>>csr.UartIP)&2 != 0 {
-			val = uart.reg >> csr.UartRX & 0xFF
-			uart.reg &^= 2<<csr.UartIP | 0xFF<<csr.UartRX
+		if !write && (cpu.CSR.Uart>>csr.UartIP)&2 != 0 {
+			val = cpu.CSR.Uart >> csr.UartRX & 0xFF
+			cpu.CSR.Uart &^= 2<<csr.UartIP | 0xFF<<csr.UartRX
 
 			uart.sync()
 		}
 
 	case 0x0200_0010: // ie
-		val = uart.reg >> csr.UartIE & 3
+		val = cpu.CSR.Uart >> csr.UartIE & 3
 
 		if write {
-			uart.reg = uart.reg&^(3<<csr.UartIE) | writeVal&3<<csr.UartIE
+			cpu.CSR.Uart = cpu.CSR.Uart&^(3<<csr.UartIE) | writeVal&3<<csr.UartIE
 
 			uart.sync()
 		}
 
 	case 0x0200_0014: // ip
-		val = uart.reg >> csr.UartIP & 3
+		val = cpu.CSR.Uart >> csr.UartIP & 3
 
 	case 0x0320_0004: // PLIC claim
-		if (uart.reg>>csr.UartIP)&(uart.reg>>csr.UartIE)&3 != 0 {
+		if (cpu.CSR.Uart>>csr.UartIP)&(cpu.CSR.Uart>>csr.UartIE)&3 != 0 {
 			val = 1
 		}
 	}
@@ -62,30 +62,30 @@ func (uart *UART) Access(addr int, width int, write bool, writeVal int) int {
 }
 
 func (uart *UART) HasOutput() bool {
-	return uart.reg>>csr.UartIP&1 == 0
+	return uart.cpu.CSR.Uart>>csr.UartIP&1 == 0
 }
 
 func (uart *UART) GetOutput() byte {
-	b := byte(uart.reg >> csr.UartTX)
+	b := byte(uart.cpu.CSR.Uart >> csr.UartTX)
 
-	uart.reg = uart.reg&^(0xFF<<csr.UartTX) | 1<<csr.UartIP
+	uart.cpu.CSR.Uart = uart.cpu.CSR.Uart&^(0xFF<<csr.UartTX) | 1<<csr.UartIP
 	uart.sync()
 
 	return b
 }
 
 func (uart *UART) AcceptsInput() bool {
-	return uart.reg>>csr.UartIP&2 == 0
+	return uart.cpu.CSR.Uart>>csr.UartIP&2 == 0
 }
 
 func (uart *UART) SetInput(b byte) {
-	uart.reg = uart.reg&^(0xFF<<csr.UartRX) | int(b)<<csr.UartRX | 2<<csr.UartIP
+	uart.cpu.CSR.Uart = uart.cpu.CSR.Uart&^(0xFF<<csr.UartRX) | int(b)<<csr.UartRX | 2<<csr.UartIP
 
 	uart.sync()
 }
 
 func (uart *UART) sync() {
-	if (uart.reg>>csr.UartIP)&(uart.reg>>csr.UartIE)&3 != 0 {
+	if (uart.cpu.CSR.Uart>>csr.UartIP)&(uart.cpu.CSR.Uart>>csr.UartIE)&3 != 0 {
 		uart.cpu.CSR.Mip |= 1 << csr.MipSEIP
 	} else {
 		uart.cpu.CSR.Mip &^= 1 << csr.MipSEIP
