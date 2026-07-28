de22e17724c57c8153ebc31d2af87171727a8574
Alex Temnok <temnok@gmail.com>
2026-07-27 21:35:12 -0700

    Comments

--- csr/const.go
+++ csr/const.go
@@ -1,69 +1,63 @@
 package csr
 
 const (
-	PrivU = 0
-	PrivS = 1
-	PrivM = 3
+	Fflags = 0x001 // Floating-point FLAGS                        https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#fcsr
+	Frm    = 0x002 // Floating-point Rounding Mode                https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#norm:dyn_round_enc
+	Fcsr   = 0x003 // Floating-point Control and Status Register  https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#fcsr
 
-	// https://github.com/riscv/riscv-isa-manual/blob/main/src/priv-csrs.adoc#user-content-mcsrnames0
-	Fflags = 0x001
-	Frm    = 0x002
-	Fcsr   = 0x003
+	Sstatus    = 0x100 // Supervisor STATUS                              https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#sstatus
+	Sie        = 0x104 // Supervisor Interrupt Enable                    https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-3-supervisor-interrupt-sip-and-sie-registers
+	Stvec      = 0x105 // Supervisor Trap VECtor                         https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-2-supervisor-trap-vector-base-address-stvec-register
+	Scounteren = 0x106 // Supervisor COUNTER ENable                      https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-5-counter-enable-scounteren-register
+	Sscratch   = 0x140 // Supervisor SCRATCH                             https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-6-supervisor-scratch-sscratch-register
+	Sepc       = 0x141 // Supervisor Exception Program Counter           https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-7-supervisor-exception-program-counter-sepc-register
+	Scause     = 0x142 // Supervisor Cause                               https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#scause
+	Stval      = 0x143 // Supervisor Trap VALue                          https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-9-supervisor-trap-value-stval-register
+	Sip        = 0x144 // Supervisor Interrupt Pending                   https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-3-supervisor-interrupt-sip-and-sie-registers
+	Stimecmp   = 0x14D // Supervisor TIMEr CoMPare                       https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#stimecmp
+	Satp       = 0x180 // Supervisor Address Translation and Protection  https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#satp
 
-	Sstatus    = 0x100
-	Sie        = 0x104
-	Stvec      = 0x105
-	Scounteren = 0x106
-	Sscratch   = 0x140
-	Sepc       = 0x141
-	Scause     = 0x142
-	Stval      = 0x143
-	Sip        = 0x144
-	Stimecmp   = 0x14D
-	Satp       = 0x180
+	Mstatus       = 0x300 // Machine STATUS                     https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-6-machine-status-mstatus-and-mstatush-registers
+	Misa          = 0x301 // Machine ISA                        https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#misa
+	Medeleg       = 0x302 // Machine Exception DELEGation       https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-8-machine-trap-delegation-medeleg-and-mideleg-registers
+	Mideleg       = 0x303 // Machine Exception DELEGation       https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-8-machine-trap-delegation-medeleg-and-mideleg-registers
+	Mie           = 0x304 // Machine Interrupt Enable           https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-9-machine-interrupt-mip-and-mie-registers
+	Mtvec         = 0x305 // Machine Trap VECtor                https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-7-machine-trap-vector-base-address-mtvec-register
+	Mcounteren    = 0x306 // Machine COUNTER ENable             https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcounteren
+	Menvcfg       = 0x30A // Machine ENVironment ConFiGuration  https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#sec:menvcfg
+	Mcountinhibit = 0x320 // Machine COUNTer INHIBIT            https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-12-machine-counter-inhibit-mcountinhibit-register
+	Mscratch      = 0x340 // Machine SCRATCH                    https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-13-machine-scratch-mscratch-register
+	Mepc          = 0x341 // Machine Exception Program Counter  https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-14-machine-exception-program-counter-mepc-register
+	Mcause        = 0x342 // Machine CAUSE                      https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcause
+	Mtval         = 0x343 // Machine Trap VALue                 https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-16-machine-trap-value-mtval-register
+	Mip           = 0x344 // Machine Interrupt Enable           https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-9-machine-interrupt-mip-and-mie-registers
+	Mcycle        = 0xB00 // Machine CYCLE                      https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-10-hardware-performance-monitor
+	Minstret      = 0xB02 // Machine INSTructions RETired       https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-10-hardware-performance-monitor
 
-	Mstatus       = 0x300
-	Misa          = 0x301
-	Medeleg       = 0x302
-	Mideleg       = 0x303
-	Mie           = 0x304
-	Mtvec         = 0x305
-	Mcounteren    = 0x306
-	Menvcfg       = 0x30A
-	Mcountinhibit = 0x320
-	Mscratch      = 0x340
-	Mepc          = 0x341
-	Mcause        = 0x342
-	Mtval         = 0x343
-	Mip           = 0x344
-	Mcycle        = 0xB00
-	Minstret      = 0xB02
+	Cycle   = 0xC00 // https://docs.riscv.org/reference/isa/v20260120/unpriv/counters.html#6-1-1-zicntr-extension-for-base-counters-and-timers
+	Time    = 0xC01 // https://docs.riscv.org/reference/isa/v20260120/unpriv/counters.html#6-1-1-zicntr-extension-for-base-counters-and-timers
+	Instret = 0xC02 // https://docs.riscv.org/reference/isa/v20260120/unpriv/counters.html#6-1-1-zicntr-extension-for-base-counters-and-timers
 
-	Cycle   = 0xC00
-	Time    = 0xC01
-	Instret = 0xC02
+	Mvendorid = 0xF11 // Machine VENDOR ID          https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-2-machine-vendor-id-mvendorid-register
+	Marchid   = 0xF12 // Machine ARCHitecture ID    https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-3-machine-architecture-id-marchid-register
+	Mimpid    = 0xF13 // Machine IMPlementation ID  https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-4-machine-implementation-id-mimpid-register
+	Mhartid   = 0xF14 // Machine HART ID            https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-5-hart-id-mhartid-register
 
-	Mvendorid = 0xF11
-	Marchid   = 0xF12
-	Mimpid    = 0xF13
-	Mhartid   = 0xF14
+	PrivU = 0 // PRIVilege of User        https://docs.riscv.org/reference/isa/v20260120/priv/priv-intro.html#privilege-levels
+	PrivS = 1 // PRIVilege of Supervisor  https://docs.riscv.org/reference/isa/v20260120/priv/priv-intro.html#privilege-levels
+	PrivM = 3 // PRIVilege of Machine     https://docs.riscv.org/reference/isa/v20260120/priv/priv-intro.html#privilege-levels
 
-	// https://riscv.github.io/riscv-isa-manual/snapshot/unprivileged/#bitdef
-	FflagsNX = 0
-	FflagsUF = 1
-	FflagsOF = 2
-	FflagsDZ = 3
-	FflagsNV = 4
-	FcsrRM   = 5
+	FflagsNX = 0 // iNeXact            https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#bitdef
+	FflagsUF = 1 // UnderFlow          https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#bitdef
+	FflagsOF = 2 // OverFlow           https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#bitdef
+	FflagsDZ = 3 // Divide by Zero     https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#bitdef
+	FflagsNV = 4 // iNValid operation  https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#bitdef
 
-	// https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcounteren
-	McounterenCY = 0
-	McounterenTM = 1
-	McounterenIR = 2
+	FcsrRM = 5 // Rounding Mode: https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#norm:dyn_round_enc
 
-	// https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#3-1-1-12-machine-counter-inhibit-mcountinhibit-register
-	McountinhibitCY = 0
-	McountinhibitIR = 2
+	McounterenCY = 0 // CYcle    https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcounteren
+	McounterenTM = 1 // TiMe     https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcounteren
+	McounterenIR = 2 // InstRet  https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcounteren
 
 	// https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#3-1-1-6-4-memory-privilege-in-mstatus-register
 	MstatusSIE  = 1
@@ -97,7 +91,7 @@ const (
 	UartTX   = 0
 	UartRX   = 8
 	UartIE   = 16
-	UartIP   = 24
+	UartIP   = 18
 	UartIPtx = UartIP
 	UartIPrx = UartIP + 1
 )
--- csr/registers.go
+++ csr/registers.go
@@ -1,30 +1,32 @@
 package csr
 
 type Registers struct {
-	Priv, Uart int // hidden
+	Fcsr int // Floating-point Control and Status Register  https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#fcsr
 
-	Fcsr int // https://docs.riscv.org/reference/isa/v20260120/unpriv/f-st-ext.html#fcsr
+	Mcause     int // Machine CAUSE                      https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcause
+	Mcounteren int // Machine COUNTER ENable             https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#mcounteren
+	Mcycle     int // Machine CYCLE                      https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-10-hardware-performance-monitor
+	Medeleg    int // Machine Exception DELEGation       https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-8-machine-trap-delegation-medeleg-and-mideleg-registers
+	Menvcfg    int // Machine ENVironment ConFiGuration  https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#sec:menvcfg
+	Mepc       int // Machine Exception Program Counter  https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-14-machine-exception-program-counter-mepc-register
+	Mideleg    int // Machine Interrupt DELEGation       https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-8-machine-trap-delegation-medeleg-and-mideleg-registers
+	Mie        int // Machine Interrupt Enable           https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-9-machine-interrupt-mip-and-mie-registers
+	Mip        int // Machine Interrupt Pending          https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-9-machine-interrupt-mip-and-mie-registers
+	Mscratch   int // Machine SCRATCH                    https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-13-machine-scratch-mscratch-register
+	Mstatus    int // Machine STATUS                     https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-6-machine-status-mstatus-and-mstatush-registers
+	Mtval      int // Machine Trap VALue                 https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-16-machine-trap-value-mtval-register
+	Mtvec      int // Machine Trap VECtor                https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#2-1-1-7-machine-trap-vector-base-address-mtvec-register
 
-	Mcause     int
-	Mcounteren int
-	Mcycle     int
-	Medeleg    int
-	Menvcfg    int // https://docs.riscv.org/reference/isa/v20260120/priv/machine.html#sec:menvcfg
-	Mepc       int
-	Mideleg    int
-	Mie        int
-	Mip        int
-	Mscratch   int
-	Mstatus    int
-	Mtval      int
-	Mtvec      int
+	Priv int // current PRIVilege: hidden non-addressable register
 
-	Satp       int
-	Scause     int
-	Scounteren int
-	Sepc       int
-	Sscratch   int
-	Stimecmp   int
-	Stval      int
-	Stvec      int
+	Satp       int // Supervisor Address Translation and Protection  https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#satp
+	Scause     int // Supervisor Cause                               https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#scause
+	Scounteren int // Supervisor COUNTER ENable                      https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-5-counter-enable-scounteren-register
+	Sepc       int // Supervisor Exception Program Counter           https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-7-supervisor-exception-program-counter-sepc-register
+	Sscratch   int // Supervisor SCRATCH                             https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-6-supervisor-scratch-sscratch-register
+	Stimecmp   int // Supervisor TIMEr CoMPare                       https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#stimecmp
+	Stval      int // Supervisor Trap VALue                          https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-9-supervisor-trap-value-stval-register
+	Stvec      int // Supervisor Trap VECtor                         https://docs.riscv.org/reference/isa/v20260120/priv/supervisor.html#11-1-1-2-supervisor-trap-vector-base-address-stvec-register
+
+	Uart int // UART state: custom hidden non-addressable register
 }
